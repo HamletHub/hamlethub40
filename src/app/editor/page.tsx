13743803f825d82ae32c70b7896fcdb4c2801324
kendjr <kendjr@gmail.com>
2025-04-30 11:06:24 -0400

    fixed no user in editor

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -4,7 +4,7 @@ import EditorForm from '@/app/_components/EditorForm';
 
 export default async function EditorPage() {
   const session = await getServerSession(authOptions);
-  if (!session || session.user.role !== 'admin') {
+  if (!session || session.user?.role !== 'admin') {
     return <div>You are not authorized to access this page. Please <a href="/login">log in</a>.</div>;
   }
   return <EditorForm />;
